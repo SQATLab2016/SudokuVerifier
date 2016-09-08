01513e6afdc23c94495b2c00d428e52db02ffe5e
gruizsan <gruizsan@STUPID-PC723.stnet.local>
2016-09-08 16:59:52 +0300

    Still not finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,9 +2,27 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		
+		if(candidateSolution.length()!=81) return -5;
+		
+		Boolean[] checker = new Boolean[9];
+		int currentNumber;
+		for(int i=0; i<81; i = i + 9){
+			initializeChecker(checker);
+			for(int j=i; j<i+9; j++){
+				currentNumber = (int) candidateSolution.charAt(j);
+				if(checker[currentNumber-1]==false) checker[currentNumber-1]=true;
+				else return -3;
+			}
+		}
+		
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
 		//check
+	}
+	
+	private void initializeChecker(Boolean[] checker){
+		for(int i = 0; i<9; i++) checker[i]=false;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -8,8 +10,40 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerify_CorrectSolution_Returns_0() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+				
+		// Act
+		int result = verifier.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+				
+		// Assert
+		assertEquals(0, result);
 	}
+	
+	@Test
+	public void testVerify_WrongFormat_Returns_Minus5() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+				
+		// Act
+		int result = verifier.verify("41736982563215894795872431682543716979158643234691275828964357157329168416487529");
+				
+		// Assert
+		assertEquals(-5, result);
+	}
+	
+	@Test
+	public void testVerify_WrongBecauseOfRow_Returns_Minus3() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+				
+		// Act
+		int result = verifier.verify("417349825632158947958724316825437169791586432346912758289643571573291684164875293");
+				
+		// Assert
+		assertEquals(-5, result);
+	}
+
 
 }
