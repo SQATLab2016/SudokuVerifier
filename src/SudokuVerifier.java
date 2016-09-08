67faf73270a33afa54cc2c677eb679bbc8faccd0
thoyhtya <thoyhtya@STUPID-PC502.stnet.local>
2016-09-08 16:59:34 +0300

    unfinished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,21 @@
 
 public class SudokuVerifier {
 	
-	public int verify(String candidateSolution) {
+	public static int verify(String candidateSolution) {		
+		
+		if (candidateSolution.length() != 81) //check lenght
+			return 1;
+		
+		for(int i=0; i<=81; i=i+9){
+			String row = candidateSolution.substring(i,i+9);
+			int intRow = Integer.parseInt(row);
+			
+			if (intRow != 45) //check rows
+				return 1;
+			
+		}
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
-		//check
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,16 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testLength() {
+		int result = SudokuVerifier.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		
+		assertEquals("Sudokus length", 1, result);	
 	}
 
+	@Test
+	public void testRow() {
+		int result = SudokuVerifier.verify("17369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		
+		assertEquals("Sudokus length", 1, result);	
+	}
 }
