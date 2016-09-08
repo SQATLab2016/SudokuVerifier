abfb5d1c21b831cae85de156fea75180b6be651a
sampelto <sampelto@STUPID-PC717.stnet.local>
2016-09-08 16:58:48 +0300

    First tests and some implementation

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,8 +3,75 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		
+		//check that candidate solution has exactly 9*9 chars
+		if (candidateSolution.length() != 81) {
+			return -1;
+		}
+		
+		if (checkCandidateForInvalidChars(candidateSolution) == -1) {
+			return -1;
+		}
+		
+		
+		//split candidate string to global rows -> string array
+		String[] globalRows = splitCandidateToGlobalRows(candidateSolution);
+		
+		//use global rows to create global columns -> string array
+		String[] globalColumns = createGlobalColumnsFromGlobalRows(globalRows);
+		
+		//use candidate string to create subgrids -> string array
+		String[] subgrids = createSubgridsFromCandidate(candidateSolution);
+		
+		if (checkSudokuArray(globalRows) != 0) {
+			return checkSudokuArray(globalRows);
+		}
+		
+		if (checkSudokuArray(globalColumns) != 0) {
+			return checkSudokuArray(globalColumns);
+		}
+		
+		if (checkSudokuArray(subgrids) != 0) {
+			return checkSudokuArray(subgrids);
+		}
+		
 		return 0;
 		
 		//check
+	}
+	
+	private String[] splitCandidateToGlobalRows(String candidateSolution) {
+		String[] globalRows = new String[9];
+		String tempString;
+		
+		for(int i = 0; i < 9; i++) {
+			if(i == 0) tempString = candidateSolution.substring(i, i * 9 - 1);
+			
+		}
+		//Implement
+		return globalRows;
+	}
+	
+	private String[] createGlobalColumnsFromGlobalRows(String[] globalRows) {
+		String[] globalColumns = new String[9];
+		//Implement
+		return globalColumns;
+	}
+	
+	private String[] createSubgridsFromCandidate(String candidateSolution) {
+		String[] subgrids = new String[9];
+		//Implement
+		return subgrids;
+	}
+	
+	private int checkSudokuArray(String[] sudokuArray) {
+		return 0;
+	}
+	
+	private int checkCandidateForInvalidChars(String candidateSolution) {
+		if (candidateSolution.matches("[0-9]+")) {
+			return 0;
+		}
+		else return -1;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,31 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerify_StringWithTooManyChars() {
+		String testCandidate = "4173698256321589479587243168254371697915864323469127582896435715732916841648752935";
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		int result = verifier.verify(testCandidate);
+		assertEquals("String has too many chars", -1, result);
 	}
+	
+	@Test
+	public void testVerify_StringWithTooLittleChars() {
+		String testCandidate = "41736982563215894795872431682543716979158643234691275828964357157329168416487529";
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		int result = verifier.verify(testCandidate);
+		assertEquals("String has too little chars", -1, result);
+	}
+	
+	@Test
+	public void testVerify_StringWithIllegalChars() {
+		String testCandidate = "4173698256321589479587243168254371697915864323469127582896435715732916841648752-3";
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		int result = verifier.verify(testCandidate);
+		assertEquals("String has invalid chars", -1, result);
+	}
+	
 
 }
