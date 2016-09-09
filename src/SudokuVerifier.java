62b48d495a6a521c447934214a783a4d05d0d539
heiheisk <heiheisk@STUPID-PC727.stnet.local>
2016-09-09 16:50:11 +0300

    good start.

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,9 +2,27 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
 		
-		//check
+		// if the string has only integers
+		if(candidateSolution.matches("^[0-9]+$") && candidateSolution.length() == 81) {
+			
+			// loop through all of the nine rows.
+			for(int i = 0; i < 9; i++) {
+				
+				// create substring
+				String subString = candidateSolution.substring(i, i+9);
+				
+				// iterate through substring
+				for (char c : subString.toCharArray()) {
+					
+				}
+			}
+			
+			// solution is correct.
+			return 0;
+		}
+		
+		// if the solution string contains chars or it's length is not 81.
+		return -1;
 	}
 }
