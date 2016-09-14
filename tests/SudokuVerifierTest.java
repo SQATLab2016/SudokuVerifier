4677fb2732cc827ce8207252928113f0459933f8
somename <someemail>
2016-09-14 21:57:13 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -34,7 +34,7 @@ public class SudokuVerifierTest {
 		// Act
 		int result = sv.verify("017319825632158947958724316825437169791586432346912758289643571573291684164875293");
 		// Assert
-		assertEquals(result, -1);
+		assertEquals(result, -3);
 	}
 	
 	@Test
