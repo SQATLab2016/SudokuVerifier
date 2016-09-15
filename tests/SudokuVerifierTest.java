ed6f65cfd52af76728f0901303c8bd93cc1775b1
somename <someemail>
2016-09-15 15:07:27 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -13,4 +13,17 @@ public class SudokuVerifierTest {
 		
 	}
 
+	
+	
+	
+	@Test
+	public void testVerifyDigitsCorrectString() {
+		
+		SudokuVerifier sf= new SudokuVerifier();
+		
+		assertTrue(		sf.verifyDigits("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+		
+	}
+	
+	
 }
