cdb633a808f5ebd5cc965c7c4e80964169c9e4b3
somename <someemail>
2016-09-15 15:08:23 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -19,6 +19,13 @@ public class SudokuVerifierTest {
 		assertTrue(		sf.verifyDigits("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
 		
 	}
-	
+	@Test
+	public void testVerifyDigitsIncorrectString() {
+		
+		SudokuVerifier sf= new SudokuVerifier();
+		
+		assertTrue(		sf.verifyDigits("123456789912345678891234567789123456678912345567891234456789123345678912234567891"));
+		
+	}
 	
 }
