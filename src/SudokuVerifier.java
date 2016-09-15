0aa4e0fcda10394ee7122215692ec0e8e16d0f81
somename <someemail>
2016-09-15 14:50:38 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -14,9 +14,15 @@ public class SudokuVerifier {
 		return 0;
 	}
 	private int SpliceStringToMap(String str){
+		int index = 0;
 		for (int row=0; row<9; row++){
 			for (int column=0; column<9; column++){
-				
+				char value = str.charAt(index);
+				int cellValue = this.GetNumberFromString(value);
+				if (cellValue <= 0 && cellValue > 9){
+					return -1;
+				}
+				index++;
 			}
 		}
 		
