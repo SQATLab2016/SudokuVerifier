61903f43df95b01751c6fe5840c7d2d28c733e40
somename <someemail>
2016-09-15 16:56:43 +0300

    besouro automatic message

--- src/Board.java
+++ src/Board.java
@@ -4,5 +4,15 @@ public class Board {
 	
 	 ArrayList<ArrayList> board = new ArrayList<ArrayList>(18);
 	 
+	 private void insertRow(ArrayList<Integer> row) {
+	    	
+			board.add(row);
+		}
+	 
+	 private void insertColumn(ArrayList<Integer> column) {
+	    	
+			board.add(column);
+		}
+	  
 
 }
