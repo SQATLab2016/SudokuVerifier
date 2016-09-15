298863652d315389fe26b9e062038d28a4001db8
somename <someemail>
2016-09-15 16:42:33 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -36,31 +36,28 @@ public class SudokuVerifierTest {
 	@Test
 	public void testColumnOK() {
 		int[][] matrix = SudokuVerifier.toM2("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 9);
-		int rowOrder = SudokuVerifier.verifyColumn(matrix);
+		int result = SudokuVerifier.verifyColumn(matrix);
 		
-		//int result = rowNumber + rowOrder;
 		
-		assertEquals("Sudokus column OK", 0, rowOrder);	
+		assertEquals("Sudokus column OK", 0, result);	
 	}
 
 	@Test
 	public void testColumnER() {
 		int[][] matrix = SudokuVerifier.toM2("789634571573291684164875293825347169791586432346912758417469825632158947958723316", 9);
-		int rowOrder = SudokuVerifier.verifyColumn(matrix);
+		int result = SudokuVerifier.verifyColumn(matrix);
 		
-		//int result = rowNumber + rowOrder;
 		
-		assertEquals("Sudokus column ER", 1, rowOrder);	
+		assertEquals("Sudokus column ER", 1, result);	
 	}
 	
 	@Test
 	public void test3x3OK() {
 		int[][] matrix = SudokuVerifier.toM2("417469825632158947958723316825347169791586432346912758289634571573291684164875293", 9);
-		int rowOrder = SudokuVerifier.verify3x3(matrix);
+		int result = SudokuVerifier.verify3x3(matrix);
 		
-		//int result = rowNumber + rowOrder;
 		
-		assertEquals("Sudokus 3x3 OK", 0, rowOrder);	
+		assertEquals("Sudokus 3x3 OK", 0, result);	
 	}
 	
 	@Test
@@ -68,7 +65,6 @@ public class SudokuVerifierTest {
 		int[][] matrix = SudokuVerifier.toM2("289613571573294684164875293825437169791586432346942758417369825632158947958721316", 9);
 		int rowOrder = SudokuVerifier.verify3x3(matrix);
 		
-		//int result = rowNumber + rowOrder;
 		
 		assertEquals("Sudokus 3x3 ER", 1, rowOrder);	
 	}
