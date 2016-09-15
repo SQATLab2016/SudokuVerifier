85e6c40bc0ac1e699aaf614ce5743eb6704dd290
somename <someemail>
2016-09-15 16:22:56 +0300

    besouro automatic message

---
+++ src/Square.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+public class Square {
+	
+	@SuppressWarnings("unused")
+	private void saveNumbers(int input) {
+		
+        ArrayList<Integer> positions = new ArrayList<Integer>();
+	    for (int i = 0; i < 9; i++)
+	        positions.add(input);
+	}
+	
+	
+	
+	
+}
