c6fbcad01acd7fdfafe99c0d50917ebd02448cdf
somename <someemail>
2016-09-15 16:31:03 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -26,7 +26,8 @@ public class SudokuVerifierTest {
 	
 	@Test
 	public void testRowER() {
-		int[][] matrix = SudokuVerifier.toM2("4217369825632158947958724316825437169791586432346912758289643571573291684164875293", 9);
+		int[][] matrix = SudokuVerifier.toM2("417469825632158947958723316825347169791586432346912758289634571573291684164875293
+", 9);
 		int result = SudokuVerifier.verifyRow(matrix);
 	
 		
@@ -42,7 +43,7 @@ public class SudokuVerifierTest {
 		
 		assertEquals("Sudokus row OK", 0, rowOrder);	
 	}
-	
+
 	@Test
 	public void testColumnER() {
 		int[][] matrix = SudokuVerifier.toM2("123456789912345678891234567789123456678912345567891234456789123345678912234567891", 9);
