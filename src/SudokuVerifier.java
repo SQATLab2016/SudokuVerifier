a717af90cd20c2ab819e568bfb3e7354deb97dc2
somename <someemail>
2016-09-15 15:59:03 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -12,7 +12,14 @@ public class SudokuVerifier {
 		// Checks if every char is a positive number
 		for(int i=0; i<81; i++){
 			currentChar = candidateSolution.charAt(i);
-			if(!Character.isDigit(currentChar) && currentChar==0) return -1;
+			if(!Character.isDigit(currentChar) || currentChar=='0') return -1;
+		}
+		
+		//Checks if all sub-grids are valid
+		for(int i = 0; i<8; i = i + 3){
+			for(int j = i; j<i+3; j++){
+				for(int k = j; )
+			}
 		}
 		
 		// Checks if all rows are valid
