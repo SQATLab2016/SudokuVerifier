61256eb85c39c8cc41e7830487efa3d880179395
somename <someemail>
2016-09-15 16:21:54 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -36,23 +36,23 @@ public class SudokuVerifier {
 			return 1;
 	}
 	
-	public static int verifyRow(String candidateSolution) {		
-		
-		int allNumbers = 0;
-		
-		for(int i=0; i<=81; i=i+9){
-			String row = candidateSolution.substring(i,i+9);
-			int intRow = Integer.parseInt(row);
-		
-			if (intRow != 45) //check rows
-				allNumbers = 1;		
+	
+	public static int verifyRow(int[][] candidateSolution) {		
+			
+		for(int i=0; i<9; i++){
+			BitSet filled = new BitSet(9);
+			for(int j=0; j<9; j++){
+				if(filled.get(candidateSolution[i][j]-1))
+					return 1;
+				else 
+					filled.set(candidateSolution[i][j]-1);
+			}
 		}
-		return  allNumbers;
-		
+		return 0;
 	}
 	
-public static int verifyRowOrder(int[][] candidateSolution) {		
-			
+	public static int verifyColum(int[][] candidateSolution) {		
+		
 		for(int i=0; i<9; i++){
 			BitSet filled = new BitSet(9);
 			for(int j=0; j<9; j++){
