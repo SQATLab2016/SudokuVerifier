09413b985f607b60c3a9df46e4ba2ebb2a36f8f5
somename <someemail>
2016-09-15 16:27:49 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -16,7 +16,7 @@ public class SudokuVerifierTest {
 	}
 
 	@Test
-	public void testRow() {
+	public void testRowOK() {
 		int[][] matrix = SudokuVerifier.toM2("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 9);
 		int result = SudokuVerifier.verifyRow(matrix);
 	
@@ -25,8 +25,27 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
-	public void testColumn() {
+	public void testRowER() {
+		int[][] matrix = SudokuVerifier.toM2("4217369825632158947958724316825437169791586432346912758289643571573291684164875293", 9);
+		int result = SudokuVerifier.verifyRow(matrix);
+	
+		
+		assertEquals("Sudokus row OK", 0, result);	
+	}
+	
+	@Test
+	public void testColumnOK() {
 		int[][] matrix = SudokuVerifier.toM2("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 9);
+		int rowOrder = SudokuVerifier.verifyColumn(matrix);
+		
+		//int result = rowNumber + rowOrder;
+		
+		assertEquals("Sudokus row OK", 0, rowOrder);	
+	}
+	
+	@Test
+	public void testColumnER() {
+		int[][] matrix = SudokuVerifier.toM2("123456789912345678891234567789123456678912345567891234456789123345678912234567891", 9);
 		int rowOrder = SudokuVerifier.verifyColumn(matrix);
 		
 		//int result = rowNumber + rowOrder;
