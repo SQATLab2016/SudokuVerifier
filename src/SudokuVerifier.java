79ad76662f06170e96a736ac388765b0e2effc16
somename <someemail>
2016-09-15 15:57:41 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -39,7 +39,7 @@ public static int verifyRowOrder(String candidateSolution) {
 			BitSet filled = new BitSet(9);
 			for(int j=1; j<8; j++){
 				filled.set(candidateSolution.charAt(i*j-1)-1);
-			if(filled.size() != 9)
+			if(filled.length() != 9)
 				return 1;
 			}
 		}
