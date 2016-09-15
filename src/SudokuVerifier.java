fea785babbf35aa18ee034817a8efaaf305d2b12
somename <someemail>
2016-09-15 14:45:35 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,25 @@
+import java.util.Map;
 
 public class SudokuVerifier {
 	
+	Map<Integer, Integer> sudokuMapping;
+	
 	public int verify(String candidateSolution) {
+		
+		
+		
+		
+		
 		// returns 0 if the candidate solution is correct
 		return 0;
+	}
+	private int SpliceStringToMap(String str){
+		for (int row=0; row<9; row++){
+			for (int column=0; column<9; column++){
+				
+			}
+		}
 		
-		//check
+		return 0;
 	}
 }
