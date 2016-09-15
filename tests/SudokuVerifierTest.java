72ffec869508f6384a5a9b2bdf2bf9624627e691
somename <someemail>
2016-09-15 15:46:51 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -40,7 +40,25 @@ public class SudokuVerifierTest {
 		
 		SudokuVerifier sf= new SudokuVerifier();
 		
-		assertTrue(		sf.verifyRow()"111116789912345678891234567789123456678912345567891234456789123345678912234567891"));
+		assertTrue(		sf.verifyRow("111116789912345678891234567789123456678912345567891234456789123345678912234567891"));
+		
+	}
+	
+	
+	@Test
+	public void testVerifyColumnCorrectString() {
+		
+		SudokuVerifier sf= new SudokuVerifier();
+		
+		assertTrue(		sf.verifyColumns("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+		
+	}
+	@Test
+	public void testVerifyColumnIncorrectString() {
+		
+		SudokuVerifier sf= new SudokuVerifier();
+		
+		assertTrue(		sf.verifyColumns("417369824442158947958724316825437169791586432346912758289643571573291684164875293"));
 		
 	}
 	
