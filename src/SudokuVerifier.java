af09ead1865a346d26670998c912f5e5f8e79bad
somename <someemail>
2016-09-15 15:34:09 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -7,29 +7,36 @@ public class SudokuVerifier {
 		
 		Boolean[] checker = new Boolean[9];
 		int currentNumber;
+		char currentChar;
 		
-		//Checks if all columns are valid
-		for(int i = 0; i < 9; i++){
-			initializeChecker(checker);
-			for(int j = i; j < 81; j = j + 9){
-				currentNumber = (int) candidateSolution.charAt(j) - '0';
-				if(checker[currentNumber-1]==false) checker[currentNumber-1]=true;
-				else return -4;
-			}
+		// Checks if every char is a positive number
+		for(int i=0; i<81; i++){
+			currentChar = candidateSolution.charAt(i);
+			if(!Character.isDigit(currentChar) && currentChar==0) return -1;
 		}
 		
 		// Checks if all rows are valid
 		for(int i=0; i<81; i = i + 9){
 			initializeChecker(checker);
 			for(int j=i; j<i+9; j++){
-				currentNumber = (int) candidateSolution.charAt(j) - '0';
+				currentNumber = Character.getNumericValue(candidateSolution.charAt(j));
+				//currentNumber = (int) candidateSolution.charAt(j) - '0';
 				if(checker[currentNumber-1]==false) checker[currentNumber-1]=true;
 				else return -3;
 			}
 		}
 		
-	
-		
+		//Checks if all columns are valid
+		for(int i = 0; i < 9; i++){
+			initializeChecker(checker);
+			for(int j = i; j < 81; j = j + 9){
+				currentNumber = Character.getNumericValue(candidateSolution.charAt(j));
+				//currentNumber = (int) candidateSolution.charAt(j) - '0';
+				if(checker[currentNumber-1]==false) checker[currentNumber-1]=true;
+				else return -4;
+			}
+		}
+
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
