df5238fcabb3191f4ca64de43e4437b38af2eb98
somename <someemail>
2016-09-15 15:41:02 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -18,7 +18,7 @@ public class SudokuVerifier {
 	public boolean checkLines(String candidateSolution){
 		 String [] solution =candidateSolution.split("");
 		 int i = 0;
-		 String[] Line1 = new String[9] ;
+		 String[] Line1 = new String[9];
 		 String[] Line2 = new String[9];
 		 String[] Line3 = new String[9];
 		 String[] Line4 = new String[9];
@@ -39,12 +39,38 @@ public class SudokuVerifier {
 		return check(Line1)&&check(Line2)&&check(Line3)&&check(Line4)&&check(Line5)&&check(Line5)&&check(Line7)&&check(Line8)&&check(Line9);
 		
  }  
-	
+	public boolean checkColumns(String candidateSolution){
+		String[] solution = candidateSolution.split("");
+		int i = 0;
+		String[] Column1 = new String[9];
+		String[] Column2 = new String[9];
+		String[] Column3 = new String[9];
+		String[] Column4 = new String[9];
+		String[] Column5 = new String[9];
+		String[] Column6 = new String[9];
+		String[] Column7 = new String[9];
+		String[] Column8 = new String[9];
+		String[] Column9 = new String[9];
+		for(i=0;i<9; i++){Column1[i]=solution[i*9];}
+		for(i=0;i<9; i++){Column2[i]=solution[1+i*9];}
+		for(i=0;i<9; i++){Column3[i]=solution[2+i*9];}
+		for(i=0;i<9; i++){Column3[i]=solution[3+i*9];}
+		for(i=0;i<9; i++){Column4[i]=solution[4+i*9];}
+		for(i=0;i<9; i++){Column5[i]=solution[5+i*9];}
+		for(i=0;i<9; i++){Column6[i]=solution[6+i*9];}
+		for(i=0;i<9; i++){Column7[i]=solution[7+i*9];}
+		for(i=0;i<9; i++){Column8[i]=solution[8+i*9];}
+		for(i=0;i<9; i++){Column9[i]=solution[9+i*9];}
+		return check(Column1)&&check(Column2)&&check(Column3)&&check(Column4)&&check(Column5)&&check(Column6)&&check(Column7)&&check(Column8)&&check(Column9);
+	}
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
 		if (candidateSolution.length()==81){
 			if (checkLines(candidateSolution)==true){
-				return 0;
+				if (checkColumns(candidateSolution)==true){
+					return 0;
+				}
+				else return -4;
 				
 			}
 			else return -3;
