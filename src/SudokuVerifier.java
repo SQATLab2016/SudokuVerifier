ff24c245d84ef5afae749e682d9100350a7f0533
somename <someemail>
2016-09-15 16:00:04 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -95,7 +95,7 @@ public class SudokuVerifier {
 		
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
-				tempString.append(getThreeCharsFromRow(globalRows[j], i, (i+1) * i));
+				tempString.append(getThreeCharsFromRow(globalRows[j], i, i+3));
 			}
 			subgrids[rowCounter] = tempString.toString();
 			tempString.setLength(0);
