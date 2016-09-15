e47cb92b8f91796717f4095ac7e7a70d105f02c1
somename <someemail>
2016-09-15 15:32:50 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -49,7 +49,7 @@ public class SudokuVerifier {
 		boolean ok=true;
 		for (int i = 0; i < 81; i++) {
 			
-			if(!Character.isDigit(candidateSolution.charAt(i))){
+			if(!Character.isDigit(candidateSolution.charAt(i)) && candidateSolution.charAt(i)!='0'){
 				ok=false;
 			}
 			
@@ -62,17 +62,24 @@ public class SudokuVerifier {
 	
 		public boolean verifyRow(String candidateSolution){
 			boolean ok=true;
-			int num=0;
+			int candidateSol= Integer.parseInt(candidateSolution);
+			int numTemp1=0, numTemp2=0;;
 			for (int i = 0; i < 81; i=i+9) {
 				//row
+				boolean row= true;
 				
 				for (int j = 0; j < 9; j++) {
-					
+					numTemp1= Integer.parseInt( ""+candidateSolution.charAt(i+j));
+					for (int j2 = 0; j2 < 9; j2++) {
+						if (j!=j2 && numTemp1==Integer.parseInt( ""+candidateSolution.charAt(i+j2))) {
+							row=false;
+						}
+					}
 					
 				}
 				
 				
-				
+				if (!row) ok=false;
 			}
 			
 			
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -27,5 +27,21 @@ public class SudokuVerifierTest {
 		assertTrue(		sf.verifyDigits("123456789912345678891234567789123456678912345567891234456789123345678912234567891"));
 		
 	}
+	@Test
+	public void testVerifyRowCorrectString() {
+		
+		SudokuVerifier sf= new SudokuVerifier();
+		
+		assertTrue(		sf.verifyRow("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+		
+	}
+	@Test
+	public void testVerifyRowIncorrectString() {
+		
+		SudokuVerifier sf= new SudokuVerifier();
+		
+		assertTrue(		sf.verifyRow("123456789912345678891234567789123456678912345567891234456789123345678912234567891"));
+		
+	}
 	
 }
