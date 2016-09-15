67a240e59b2c6d86c0d7bbf261dbcce915159e22
somename <someemail>
2016-09-15 16:09:08 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -38,7 +38,7 @@ public class SudokuVerifierTest {
 	@Test
 	public void testcheckLines_True(){
 		SudokuVerifier SV=new SudokuVerifier();
-		String solution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		String solution = "123456789";
 		boolean checklines = SV.checkLines(solution);
 		assertEquals("The lines are checked false despite being true", true, checklines);
 	}
