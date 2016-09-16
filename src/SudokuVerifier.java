b3eeef732276ace7ba830800cc27042071d741b1
somename <someemail>
2016-09-16 15:26:40 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,8 +3,24 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		if (onlyPositives(candidateSolution) == -1){
+			return -1;
+		}
+		
 		return 0;
 		
 		//check
+	}
+	public int onlyPositives(String candidateSolution){
+		
+		String[] solution = candidateSolution.split("");
+		
+		if (solution.length != 81){
+			return -1;
+		}
+		else{
+			return 0;
+		}
+		
 	}
 }
