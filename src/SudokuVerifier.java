bae4a94aac5fd45d9f058b9aebf2abd54f3eeb3c
somename <someemail>
2016-09-16 15:38:15 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -5,6 +5,14 @@ public class SudokuVerifier {
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
+		// Return -1 if violating Rule #1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+		
+		// Return -2 if violating Rule #2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
+		
+		// Return -3 if violating Rule #3: A digit can appear only once in the rows of the global grid.
+		
+		// Return -4 if violating Rule #4: A digit can appear only once in the columns of the global grid.
+		
 		//check
 	}
 }
