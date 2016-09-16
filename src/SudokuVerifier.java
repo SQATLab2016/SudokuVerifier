94bd1022c3273ffec71b7adb327baad9145dd68b
somename <someemail>
2016-09-16 16:00:23 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,16 +3,13 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		
-		// returns 0 if the candidate solution is correct
-		if (candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293"))
-			return 0;
-		else
-			return -1;
-		
-		/* test positive digits */
-		/*for (int i=0;i < candidateSolution.length;i++) 
-		{ 
-		    System.out.println(candidateSolution.substring(i,i)); 
-		} */
+	      int n=0;
+	      for (char c : candidateSolution.toCharArray()) {
+	    	  if (Character.isDigit(c) == false) {
+	    		  return -1;
+	    	  }
+	      }
+	      
+	      return 0;
 	}
 }
