36179268100b131e54ed1f95d16dfdbf6cb02277
somename <someemail>
2016-09-16 14:44:51 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,14 +2,25 @@
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
 		if (candidateSolution.length() != 81) {
 			return 1;
 		} else {
 			char[] arrayChars = candidateSolution.toCharArray();
 			for (int loop = 0; loop < arrayChars.length; loop++) {
+				try {
+					int number = Integer.parseInt("" + arrayChars[loop]);
+					if (number >= 0) {
+
+					} else {
+						return 1;
+					}
+				} catch (NumberFormatException ne) {
+					ne.printStackTrace();
+					return 1;
+				}
 			}
 			return 0;
 		}
 	}
 }
+// returns 0 if the candidate solution is correct
