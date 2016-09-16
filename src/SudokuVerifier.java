67153a4c1f913c2018bc460ea94ffd1f3a9de7b8
somename <someemail>
2016-09-16 15:41:20 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -16,13 +16,13 @@ public class SudokuVerifier {
 			// returns 0 if the candidate solution is correct
 		forCheck = forCheck.substring(1);
 		}
-		char[] numbers= {0,1,2,3,4,5,6,7,8,9};
+		char[] numbers= {0,3,5,3,4,5,6,7,8,9};
 		
 		char[] firstGrid = {candidateSolution.charAt(0),candidateSolution.charAt(1),candidateSolution.charAt(2),
 				candidateSolution.charAt(9),candidateSolution.charAt(10),candidateSolution.charAt(11),
 				candidateSolution.charAt(18),candidateSolution.charAt(19),candidateSolution.charAt(20)};
-		Arrays.sort(firstGrid);
-		System.out.println(firstGrid.toString());
+		Arrays.sort(numbers);
+		System.out.println(numbers.toString());
 	    if(!numbers.equals(firstGrid)) return -2; 	
 		return 0;
 		//check
