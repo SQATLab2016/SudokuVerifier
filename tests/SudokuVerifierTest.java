7dba5351e20be3a902eef764e7888b3ba61ec9bf
somename <someemail>
2016-09-16 16:18:47 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,7 +10,7 @@ public class SudokuVerifierTest {
 	@Test
 	public void testVerify() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String candidateSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		String candidateSolution = "1-3456789912345678891234567789123456678912345567891234456789123345678912234567891";
 		
 		int result = verifier.verify(candidateSolution);
 
