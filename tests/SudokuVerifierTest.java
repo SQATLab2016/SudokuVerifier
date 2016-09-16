3a0b1c486ec8921b12751233aa355d1d0eb02fba
somename <someemail>
2016-09-16 17:06:20 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,12 +8,51 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
+	public void test0() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = verifier.verify(candidateSolution);
+		assertTrue(result == 0);
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
 	public void testR1() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String candidateSolution = "";
-		String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		int result = verifier.verify(correctSolution);
+		String candidateSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int result = verifier.verify(candidateSolution);
 		assertTrue(result == -1);
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testR2() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = verifier.verify(candidateSolution);
+		assertTrue(result == -2);
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testR3() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = verifier.verify(candidateSolution);
+		assertTrue(result == -3);
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testr4() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = verifier.verify(candidateSolution);
+		assertTrue(result == -4);
 		
 		fail("Not yet implemented");
 	}
