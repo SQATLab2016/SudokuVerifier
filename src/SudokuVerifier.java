89c647aee8918d242c3cf790cf04abe4023f1f59
somename <someemail>
2016-09-16 15:17:08 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -32,7 +32,9 @@ public class SudokuVerifier {
 		
 		for(int i = xLimit; i < xLimit+3; i++){
 			for(int j = yLimit; j < yLimit+3; j++){
-				
+				if(sudoku[i][j] == toTest && sudoku[i][j] != sudoku[x][y]){
+					
+				}
 			}
 		}
 		
