96ca09ab8808a727e8d8137b7ac7664b90e9d25d
somename <someemail>
2016-09-16 16:35:55 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -21,5 +21,13 @@ public class SudokuVerifierTest {
 		int result= verifier.verify(candidateSolution);
 		assertTrue(result == -2); 
 	}
+	@Test
+	public void testVerifyTrue() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution ="417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result= verifier.verify(candidateSolution);
+		assertTrue(result == 0); 
+	}
+	
 	
 }
