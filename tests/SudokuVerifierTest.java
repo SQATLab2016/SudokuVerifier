975c904d286949a37f91782d6f4c2eaa1bcecc81
somename <someemail>
2016-09-16 15:49:27 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -14,10 +14,11 @@ public class SudokuVerifierTest {
 		assertEquals("line not valid", 0, n.verify(correct));
 	}
 	@Test
-	public void testVerify() {
+	public void testVerifyFail() {
 		SudokuVerifier n = new SudokuVerifier();
-		String correct = 123456789912345678891234567789123456678912345567891234456789123345678912234567891;
-		assertEquals("line not valid", 0, n.verify(correct));
+		String correct = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int solution = n.verify(correct);
+		assertTrue()
 	}
 
 }
