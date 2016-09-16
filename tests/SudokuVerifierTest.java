21c960a11445c55ba2bb641eaa5b6de1ef2d0ccd
somename <someemail>
2016-09-16 16:09:10 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -39,7 +39,7 @@ public class SudokuVerifierTest {
 	public void testFirstRowNumbers(){
 		SudokuVerifier verifier = new SudokuVerifier();
 		int result = verifier.checkDuplicates("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
-		assertEquals("81 numbers", 9, result);
+		assertEquals("length of the first row:", 9, result);
 	}
 	
 }
