47a027446b04974389fe377b16664e202818d377
somename <someemail>
2016-09-16 15:46:53 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,13 +2,17 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		
 		// returns 0 if the candidate solution is correct
-		return 0;
+		if (candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293"))
+			return 0;
+		else
+			return -1;
 		
 		/* test positive digits */
-		for (int i=0;i < candidateSolution.length;i++) 
+		/*for (int i=0;i < candidateSolution.length;i++) 
 		{ 
 		    System.out.println(candidateSolution.substring(i,i)); 
-		} 
+		} */
 	}
 }
