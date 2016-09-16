50d1033594349534384d2b9756f8e1244d8806e5
somename <someemail>
2016-09-16 15:35:55 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -15,9 +15,15 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
-	public void testVerify_String2_minus1() throws StringLengthNotEqual_81Exception{
+	public void testVerify_String2_minus2() throws StringLengthNotEqual_81Exception{
 		SudokuVerifier Sudoku = new SudokuVerifier();
 		int t = Sudoku.verify("123456789912345678891234567789123456678912345567891234456789123345678912234567891");
-		assertEquals(0,t);
+		assertEquals(-2,t);
+	}
+	
+	@Test ( expected=StringLengthNotEqual_81Exception.class )
+	public void testVerify_throwException() throws StringLengthNotEqual_81Exception{
+		SudokuVerifier Sudoku = new SudokuVerifier();
+		Sudoku.verify("1234567890");
 	}
 }
