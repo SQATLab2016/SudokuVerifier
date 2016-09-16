3c4bba3fa64e4ce8dd4251cbeef5e526ee5d5a7c
somename <someemail>
2016-09-16 16:18:01 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,15 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testSudoku() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		int result = verifier.verify(candidateSolution);
+
+		
+		assertTrue("String is wrong!", result == 0 );
+		//fail("Not yet implemented");
 	}
 
 	@Test
