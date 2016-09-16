53d9f1d9fd84fa2db0a5d3d4afea1e68bbd76cec
somename <someemail>
2016-09-16 15:44:41 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,23 +8,23 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
-	
+	String corrSol = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String inorrSol = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
 	String row = "123456789";
-	String wrongSol = "";
+	//String wrongSol = "";
 	SudokuVerifier sv = new SudokuVerifier();
 	
 	public SudokuVerifierTest() {
+		/*
 		for (int i=0; i<8; i++) {
 			wrongSol.concat(row);
 		}
+		*/
 	}
-	
-	
 	
 	@Before
 	public void init() {
-		
 	}
 	
 	@Test
@@ -45,7 +45,7 @@ public class SudokuVerifierTest {
 	@Test
 	public void testValidNumberOfChar_OneCharMissing() {		
 		// remove last character from wrong solution
-		String solOneCharMissing = wrongSol.substring(0, 80);
+		String solOneCharMissing = corrSol.substring(0, 80);
 		
 		assertEquals(false, sv.validNumberOfChar(solOneCharMissing));
 	}
@@ -53,7 +53,7 @@ public class SudokuVerifierTest {
 	@Test
 	public void testValidNumberOfChar_OneCharMore() {
 		// add character to wrong solution
-		String solOneCharMore = wrongSol.concat("1");
+		String solOneCharMore = corrSol.concat("1");
 		
 		assertEquals(false, sv.validNumberOfChar(solOneCharMore));
 	}
