a1b385c41243d3bc8348dda09b38eca05dfe9468
somename <someemail>
2016-09-16 16:52:57 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -4,8 +4,6 @@ public class SudokuVerifier {
 	String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String incorrectSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
-	//public String candidateSolution;
-	
 	public int verify(String candidateSolution) {
 		
 		// Returns 0 if the candidate solution is correct	
@@ -15,6 +13,10 @@ public class SudokuVerifier {
 		return 0;
 		
 		// Return -1 if violating Rule #1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+		if (candidateSolution < 0 ) {
+			
+		}
+		return -1;
 		
 		// Return -2 if violating Rule #2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
 		
