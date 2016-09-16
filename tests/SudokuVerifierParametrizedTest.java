96f99e669ae6d37e636775583867e42e7073d761
somename <someemail>
2016-09-16 14:58:19 +0300

    besouro automatic message

--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -5,16 +5,16 @@ import org.junit.Test;
 public class SudokuVerifierParametrizedTest {
 
 	@Test
-	public void testVerticalTrue() {
+	public void testHorizontalTrue() {
 		SudokuVerifier ver = new SudokuVerifier();
 		int[] line = {1,2,3,4,5,6,7,8,9};
 		assertEquals("line not valid", true, ver.verifyVertical(3, line));
 	}
 	@Test
-	public void testVerticalFail() {
+	public void testHorizontalFail() {
 		SudokuVerifier ver = new SudokuVerifier();
 		int[] line = {1,2,3,4,4,6,7,8,9};
-		assertEquals("line not valid", fail, ver.verifyVertical(3, line));
+		assertEquals("line not valid", false, ver.verifyVertical(3, line));
 	}
 
 }
