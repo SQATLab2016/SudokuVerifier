700d8795455f21e93dd3bc34ad6046b10f30fcdd
somename <someemail>
2016-09-16 15:42:21 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -13,18 +13,18 @@ public class SudokuVerifier {
 		  			forCheck.charAt(0)=='6'|| forCheck.charAt(0)=='7' ||
 		  			forCheck.charAt(0)=='8'|| forCheck.charAt(0)=='9')) return -1;		
 			
-			// returns 0 if the candidate solution is correct
+			
 		forCheck = forCheck.substring(1);
 		}
-		char[] numbers= {0,3,5,3,4,5,6,7,8,9};
+		int[] numbers={0,1,2,3,4,5,6,7,8,9};
 		
 		char[] firstGrid = {candidateSolution.charAt(0),candidateSolution.charAt(1),candidateSolution.charAt(2),
 				candidateSolution.charAt(9),candidateSolution.charAt(10),candidateSolution.charAt(11),
 				candidateSolution.charAt(18),candidateSolution.charAt(19),candidateSolution.charAt(20)};
-		Arrays.sort(numbers);
-		System.out.println(numbers.toString());
+		Arrays.sort(firstGrid);
+		System.out.println(firstGrid.toString());
 	    if(!numbers.equals(firstGrid)) return -2; 	
 		return 0;
-		//check
+		
 	}
 }
