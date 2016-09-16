6f6ce6202dadb7e7c03dbdc44dde4862150653d1
somename <someemail>
2016-09-16 16:30:52 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -148,6 +148,72 @@ public class SudokuVerifier {
 	    return -3; 	
 	    
 		}
+		
+		char[] firstColoumn = {candidateSolution.charAt(0),candidateSolution.charAt(9),candidateSolution.charAt(18),
+				candidateSolution.charAt(27),candidateSolution.charAt(36),candidateSolution.charAt(45),
+				candidateSolution.charAt(54),candidateSolution.charAt(63),candidateSolution.charAt(72)};
+		Arrays.sort(firstColoumn);
+		
+		char[] secondColoumn = {candidateSolution.charAt(1),candidateSolution.charAt(10),candidateSolution.charAt(19),
+				candidateSolution.charAt(28),candidateSolution.charAt(37),candidateSolution.charAt(46),
+				candidateSolution.charAt(55),candidateSolution.charAt(64),candidateSolution.charAt(73)};
+		Arrays.sort(secondColoumn);
+		
+		char[] thirdColoumn = {candidateSolution.charAt(2),candidateSolution.charAt(11),candidateSolution.charAt(20),
+				candidateSolution.charAt(29),candidateSolution.charAt(38),candidateSolution.charAt(47),
+				candidateSolution.charAt(56),candidateSolution.charAt(65),candidateSolution.charAt(74)};
+		Arrays.sort(thirdColoumn);
+		
+		char[] forthColoumn = {candidateSolution.charAt(3),candidateSolution.charAt(12),candidateSolution.charAt(21),
+				candidateSolution.charAt(30),candidateSolution.charAt(39),candidateSolution.charAt(48),
+				candidateSolution.charAt(57),candidateSolution.charAt(66),candidateSolution.charAt(75)};
+		Arrays.sort(forthColoumn);
+	
+		char[] fifthColoumn = {candidateSolution.charAt(4),candidateSolution.charAt(13),candidateSolution.charAt(22),
+				candidateSolution.charAt(31),candidateSolution.charAt(40),candidateSolution.charAt(49),
+				candidateSolution.charAt(58),candidateSolution.charAt(67),candidateSolution.charAt(76)};
+
+		Arrays.sort(fifthColoumn);
+		
+		char[] sixthColoumn = {candidateSolution.charAt(5),candidateSolution.charAt(14),candidateSolution.charAt(23),
+				candidateSolution.charAt(32),candidateSolution.charAt(41),candidateSolution.charAt(50),
+				candidateSolution.charAt(59),candidateSolution.charAt(68),candidateSolution.charAt(77)};
+
+		Arrays.sort(sixthColoumn);
+		
+		char[] seventhColoumn = {candidateSolution.charAt(6),candidateSolution.charAt(15),candidateSolution.charAt(24),
+				candidateSolution.charAt(33),candidateSolution.charAt(42),candidateSolution.charAt(51),
+				candidateSolution.charAt(60),candidateSolution.charAt(69),candidateSolution.charAt(78)};
+
+		Arrays.sort(seventhColoumn);
+		
+		char[] eightColoumn = {candidateSolution.charAt(7),candidateSolution.charAt(16),candidateSolution.charAt(25),
+				candidateSolution.charAt(34),candidateSolution.charAt(43),candidateSolution.charAt(52),
+				candidateSolution.charAt(61),candidateSolution.charAt(70),candidateSolution.charAt(79)};
+
+		Arrays.sort(eightColoumn);
+		
+		char[] ninethColoumn = {candidateSolution.charAt(8),candidateSolution.charAt(17),candidateSolution.charAt(26),
+				candidateSolution.charAt(35),candidateSolution.charAt(44),candidateSolution.charAt(53),
+				candidateSolution.charAt(62),candidateSolution.charAt(71),candidateSolution.charAt(80)};
+
+		Arrays.sort(ninethColoumn);
+		
+		if((!Arrays.toString(numbers).equals(Arrays.toString(firstColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(secondColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(thirdColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(forthColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(fifthColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(sixthColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(seventhColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(eightColoumn)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(ninethRow)))
+				) {
+	    	
+	    return -4; 	
+	    
+		}
+		
 	    
 		
 	    
