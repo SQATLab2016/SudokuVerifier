6924a908cfc8f751ad4eed24b13150c6d2deef59
somename <someemail>
2016-09-16 14:51:07 +0300

    besouro automatic message

--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -7,7 +7,8 @@ public class SudokuVerifierParametrizedTest {
 	@Test
 	public void testVertical() {
 		SudokuVerifier ver = new SudokuVerifier();
-		
+		String line = "123456789";
+		assertEquals("line not valid", true, ver.verityVertical(3, line.to))
 	}
 
 }
