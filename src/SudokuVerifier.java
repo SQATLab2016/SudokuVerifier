7a6220568e0715cdee57331d60c4c663443ec00d
somename <someemail>
2016-09-16 16:48:19 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -4,7 +4,7 @@ public class SudokuVerifier {
 	String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String incorrectSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
-	public String candidateSolution;
+	//public String candidateSolution;
 	
 	public int verify(String candidateSolution) {
 		
