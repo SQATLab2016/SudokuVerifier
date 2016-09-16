cb7ed193deb2c61c8d0415f1cf8fe58693ca5323
somename <someemail>
2016-09-16 15:28:05 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -21,7 +21,7 @@ public class SudokuVerifierTest {
 	@Test
 	public void test_RowColumnViolation() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		String candidateSolution = correctSudoku;
 		
 		
 		int result = verifier.verify(candidateSolution);
