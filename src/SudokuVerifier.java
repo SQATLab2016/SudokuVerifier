03dfee020c0e32d5252d7ad87e9026619eb1c5aa
somename <someemail>
2016-09-16 15:49:49 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,6 +2,27 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		static int [][] matrix = {
+				{1,2,3,4,5,6,7,8,9},{2,3,4,5,6,7,8,9,1},{3,4,5,6,7,8,9,1,2},{4,5,6,7,8,9,1,2,3},
+				{5,6,7,8,9,1,2,3,4},{6,7,8,9,1,2,3,4,5}
+				
+				
+		}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
