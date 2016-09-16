de13131734ab5bff9d92f45da47b15be97a07bc9
somename <someemail>
2016-09-16 15:31:34 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,17 +8,20 @@ public class SudokuVerifierTest {
 	public static final int RULE_2_VOILATION_RESULT = -2;
 	public static final int RULE_3_VOILATION_RESULT = -3;
 	public static final int RULE_4_VOILATION_RESULT = -4;
-	
+	public static final int APPROVAL_RESULT = 0;
+
 	SudokuVerifier sudokuVerifier = new SudokuVerifier();
-	
+
 	@Test
 	public void testVerify_LengthVerify() {
-		assertEquals("",0,sudokuVerifier.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+		assertEquals("", APPROVAL_RESULT, sudokuVerifier
+				.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
 	}
-	
+
 	@Test
-	public void testVerify_NumberCheckRule1(){
-		assertEquals("",-1,sudokuVerifier.verify("417369825032158947958720316825437169791586432346912758289643571573291684164875293"));
+	public void testVerify_NumberCheckRule1() {
+		assertEquals("", RULE_1_VOILATION_RESULT, sudokuVerifier
+				.verify("417369825032158947958720316825437169791586432346912758289643571573291684164875293"));
 	}
-	
+
 }
