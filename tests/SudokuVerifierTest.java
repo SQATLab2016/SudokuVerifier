a60100bc9b052830ae765d6a64b88800f6c8b12e
somename <someemail>
2016-09-16 15:33:14 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,7 +8,7 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify_80characters_should_return_error() {
+	public void testVerify_80characters_should_fail_rule1() {
 		SudokuVerifier verifier = new SudokuVerifier();
 		int result = verifier.verify("41736982563215894795872431682543716979158643234691275828964357157329168416487529");
 		
@@ -16,7 +16,7 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
-	public void testVerify_should_fail_with_zero(){
+	public void testVerify_string_with_0_should_fail_rule1(){
 		SudokuVerifier verifier = new SudokuVerifier();
 		int result = verifier.verify("417369825632158947958724316805437169791586432346912758289643571573291684164875293");
 		
