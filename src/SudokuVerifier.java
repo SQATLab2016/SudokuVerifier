e3695eb2f81b3874e9a97b0d4fbaf63e44b30518
somename <someemail>
2016-09-16 16:14:48 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -30,10 +30,64 @@ public class SudokuVerifier {
 				candidateSolution.charAt(9),candidateSolution.charAt(10),candidateSolution.charAt(11),
 				candidateSolution.charAt(18),candidateSolution.charAt(19),candidateSolution.charAt(20)};
 		Arrays.sort(firstGrid);
-		Arrays.sort(numbers);
 		
+		char[] secondGrid = {candidateSolution.charAt(3),candidateSolution.charAt(4),candidateSolution.charAt(5),
+				candidateSolution.charAt(12),candidateSolution.charAt(13),candidateSolution.charAt(14),
+				candidateSolution.charAt(21),candidateSolution.charAt(22),candidateSolution.charAt(23)};
+		Arrays.sort(secondGrid);
+		
+		char[] thirdGrid = {candidateSolution.charAt(6),candidateSolution.charAt(7),candidateSolution.charAt(8),
+				candidateSolution.charAt(15),candidateSolution.charAt(16),candidateSolution.charAt(17),
+				candidateSolution.charAt(24),candidateSolution.charAt(25),candidateSolution.charAt(26)};
+		Arrays.sort(thirdGrid);
+		
+		char[] forthGrid = {candidateSolution.charAt(27),candidateSolution.charAt(28),candidateSolution.charAt(29),
+				candidateSolution.charAt(36),candidateSolution.charAt(37),candidateSolution.charAt(38),
+				candidateSolution.charAt(45),candidateSolution.charAt(46),candidateSolution.charAt(47)};
+		Arrays.sort(forthGrid);
+	
+		char[] fifthGrid = {candidateSolution.charAt(30),candidateSolution.charAt(31),candidateSolution.charAt(32),
+				candidateSolution.charAt(39),candidateSolution.charAt(40),candidateSolution.charAt(41),
+				candidateSolution.charAt(48),candidateSolution.charAt(49),candidateSolution.charAt(50)};
 
-		if(!Arrays.toString(numbers).equals(Arrays.toString(firstGrid))) {
+		Arrays.sort(fifthGrid);
+		
+		char[] sixthGrid = {candidateSolution.charAt(33),candidateSolution.charAt(34),candidateSolution.charAt(35),
+				candidateSolution.charAt(42),candidateSolution.charAt(43),candidateSolution.charAt(44),
+				candidateSolution.charAt(51),candidateSolution.charAt(52),candidateSolution.charAt(53)};
+
+		Arrays.sort(sixthGrid);
+		
+		char[] seventhGrid = {candidateSolution.charAt(54),candidateSolution.charAt(55),candidateSolution.charAt(56),
+				candidateSolution.charAt(63),candidateSolution.charAt(64),candidateSolution.charAt(65),
+				candidateSolution.charAt(72),candidateSolution.charAt(73),candidateSolution.charAt(74)};
+
+		Arrays.sort(seventhGrid);
+		
+		char[] eightGrid = {candidateSolution.charAt(57),candidateSolution.charAt(58),candidateSolution.charAt(59),
+				candidateSolution.charAt(66),candidateSolution.charAt(67),candidateSolution.charAt(68),
+				candidateSolution.charAt(75),candidateSolution.charAt(76),candidateSolution.charAt(77)};
+
+		Arrays.sort(eightGrid);
+		
+		char[] ninethGrid = {candidateSolution.charAt(60),candidateSolution.charAt(61),candidateSolution.charAt(62),
+				candidateSolution.charAt(69),candidateSolution.charAt(70),candidateSolution.charAt(71),
+				candidateSolution.charAt(78),candidateSolution.charAt(79),candidateSolution.charAt(80)};
+
+		Arrays.sort(ninethGrid);
+		
+	
+		
+		if((!Arrays.toString(numbers).equals(Arrays.toString(firstGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(secondGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(thirdGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(forthGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(fifthGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(sixthGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(seventhGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(eightGrid)))
+				||(!Arrays.toString(numbers).equals(Arrays.toString(ninethGrid)))
+				) {
 	    	
 	    return -2; 	
 	    }
