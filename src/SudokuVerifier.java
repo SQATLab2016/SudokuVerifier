a611f4439ea3913beecde12a93665af0a5acf661
somename <someemail>
2016-09-16 15:37:10 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -5,6 +5,11 @@ public class SudokuVerifier {
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
-		//check
+		/*
+		 * Return 0: means it is a valid Sudoku solution
+o Return -1: means it is violating Rule #1
+o Return -2: means it is violating Rule #2
+o Return -3: means it is violating Rule #3
+o Return -4: means it is violating Rule #4*/
 	}
 }
