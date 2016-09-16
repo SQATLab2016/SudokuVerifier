b89fbe70508c0f9b9694199011886945d27dc4fe
somename <someemail>
2016-09-16 15:34:05 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -7,14 +8,29 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	
+	
+	String row = "123456789";
+	SudokuVerifier sv = new SudokuVerifier();
+	
+	@Before
+	public void init() {
+		
+	}
+	
 	@Test
 	public void testVerify() {
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testNumberOfCharacters_LessCharacters() {
+	public void testValidNumberOfChar_OneRowMissing() {
+		String sol = "";
+		for (int i=0; i<8; i++) {
+			sol.concat(row);
+		}
 		
+		assertEquals(false, sv.validNumberOfChar(sol));
 	}
 
 }
