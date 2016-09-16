9b197acb1464eeeeb0b4152eac3754c2d3a1d96d
somename <someemail>
2016-09-16 15:15:54 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -7,6 +7,21 @@ public class SudokuVerifier {
 			return -5; //-5 will be any other error
 		}
 		
+		//Insert to data structure
+		Integer[][] sudokuTable = new Integer[9][9];
+		int stringIndex=0;
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				//Rule 1: check if all are positive digits
+				Integer digit = Integer.getInteger(String.valueOf(candidateSolution.charAt(stringIndex)));
+				if(digit<1 || digit >9){
+					return -1;
+				}
+				stringIndex++;
+			}
+		}
+		
+		//
 		
 		// returns 0 if the candidate solution is correct
 		return 0;
