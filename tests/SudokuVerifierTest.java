9a80b3e67298fb9090675acd0e087a8f93b8a412
somename <someemail>
2016-09-16 15:17:55 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,8 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	private SudokuVerifier verifier;
+
 	@Test
 	public void test() {
 		SudokuVerifier verifier = new SudokuVerifier();
@@ -22,7 +24,7 @@ public class SudokuVerifierTest {
 	
 	@Test 
 	public void test_RowColumnViolation() {
-		SudokuVerifier verifier = new SudokuVerifier();
+		verifier = new SudokuVerifier();
 		String candidateSolution = ""
 								+"123456789"
 								+"245678913"
