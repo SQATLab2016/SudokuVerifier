a1e746933435241ad2317ca7491ad1150ca259da
somename <someemail>
2016-09-16 15:48:30 +0300

    besouro automatic message

--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -1,9 +1,19 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith ( Parameterized.class )
 public class SudokuVerifierParametrizedTest {
-
+	private char origin;
+	private int result;
+	
+	public SudokuVerifierParametrizedTest(int result, char origin){
+		
+	}
+	
+	
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
