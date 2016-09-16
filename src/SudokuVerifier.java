5001d0f3d38cde6c99eed26162b90f57e9abe5ad
somename <someemail>
2016-09-16 16:16:49 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -17,10 +17,19 @@ public class SudokuVerifier {
 		boolean inputStringEquals81 = (candidateSolution.length() == 81) ? true : false;
 		return inputStringEquals81;
 	}
-	/*
+	
 	public boolean checkInputStringContainsOnlyPostiveInt(String candidateSolution){
-		
+		boolean isPostiveInt = false;
+		for(int i = 0; i < candidateSolution.length(); i++){
+			char character = candidateSolution.charAt(i);
+			try{
+				Integer.parseInt(String.valueOf(character));
+				isPostiveInt = true;
+			}catch(NumberFormatException ex){
+				return isPostiveInt = false;
+			}
+		}
+		return isPostiveInt;
 	}
-	*/
 	
 }
