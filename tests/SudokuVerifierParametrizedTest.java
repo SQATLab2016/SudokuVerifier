4bcfe86262fad89ed16d6e24cdc7befdd210cafb
somename <someemail>
2016-09-16 15:22:19 +0300

    besouro automatic message

--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -8,14 +8,30 @@ public class SudokuVerifierParametrizedTest {
 	@Test
 	public void testHorizontalTrue() {
 		SudokuVerifier ver = new SudokuVerifier();
-		int[] line = {1,2,3,4,5,6,7,8,9};
-		assertEquals("line not valid", true, ver.verifyHorizontal(3, 3 line));
+		int[][] line = {{1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9}};
+		assertEquals("line not valid", true, ver.verifyHorizontal(0, 3, line));
 	}
 	@Test
 	public void testHorizontalFail() {
 		SudokuVerifier ver = new SudokuVerifier();
-		int[] line = {1,2,3,4,4,6,7,8,9};
-		assertEquals("line not valid", false, ver.verifyHorizontal(3, 3 line));
+		int[][] line = {{1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9},
+				   {1,2,3,4,4,6,7,8,9}};
+		assertEquals("line not valid", false, ver.verifyHorizontal(0, 3, line));
 	}
 	@Test
 	public void testVerticalFail() {
