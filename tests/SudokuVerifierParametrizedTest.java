f1ae22529111d18b12252c61b9aee03c6db8e64c
somename <someemail>
2016-09-16 15:05:28 +0300

    besouro automatic message

--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -30,4 +30,18 @@ public class SudokuVerifierParametrizedTest {
 					   {1,2,3,4,4,6,7,8,9}};
 		assertEquals("line not valid", false, ver.verifyVertical(3, 2, line));
 	}
+	@Test
+	public void testVerticalTrue() {
+		SudokuVerifier ver = new SudokuVerifier();
+		int[][] line = {{1,2,3,4,4,6,7,8,9},
+					   {2,2,3,4,4,6,7,8,9},
+					   {3,2,3,4,4,6,7,8,9},
+					   {4,2,3,4,4,6,7,8,9},
+					   {5,2,3,4,4,6,7,8,9},
+					   {6,2,3,4,4,6,7,8,9},
+					   {7,2,3,4,4,6,7,8,9},
+					   {8,2,3,4,4,6,7,8,9},
+					   {9,2,3,4,4,6,7,8,9}};
+		assertEquals("line not valid", true, ver.verifyVertical(0, 0, line));
+	}
 }
