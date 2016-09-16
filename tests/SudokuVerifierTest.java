9ee8be12b04c8bd95a70b807b168522cd03db5f3
somename <someemail>
2016-09-16 15:08:55 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -8,8 +8,10 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerifyR1() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "1235648651354864132A54134321321485641321564" 
+		assertTrue
 	}
 
 }
