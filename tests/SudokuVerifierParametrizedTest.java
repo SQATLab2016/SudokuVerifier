7854322c6e9113f04d04f4e0d8c0991d07bd6400
somename <someemail>
2016-09-16 14:56:11 +0300

    besouro automatic message

--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 public class SudokuVerifierParametrizedTest {
 
 	@Test
-	public void testVertical() {
+	public void testVerticalTrue() {
 		SudokuVerifier ver = new SudokuVerifier();
 		int[] line = {1,2,3,4,5,6,7,8,9};
+		assertEquals("line not valid", true, ver.verifyVertical(3, line));
+	}
+	@Test
+	public void testVerticalFail() {
+		SudokuVerifier ver = new SudokuVerifier();
+		int[] line = {1,2,3,4,4,6,7,8,9};
 		assertEquals("line not valid", true, ver.verifyVertical(3, line));
 	}
 
