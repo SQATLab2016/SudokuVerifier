322534f9b180f72fd734fa07a5b1254b02a84fed
somename <someemail>
2016-09-16 15:42:50 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,38 +10,29 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
-	private SudokuVerifier verifier;
-	private String candidateSolution;
-	private String correctSolution;
-
+	String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String incorrectSudoku ="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
+	
+	
 	@Test
 	public void test() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String candidateSolution = "";
+		String candidateSolution = correctSudoku;
 		int result = verifier.verify(candidateSolution );
 		assertTrue(result == - 2 || result == -3);
 		
-		//
+	
 	}
 	
 	@Test 
 	public void test_RowColumnViolation() {
-		setVerifier(new SudokuVerifier());
-		setCandidateSolution(
-								"123456789"
-								+"245678913"
-								+"345678912"
-								+"456789123"
-								+"567891234"
-								+"678912345"
-								+"789123456"
-								+"891234567"
-								+"989123456");
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = correctSudoku;
 								
+												
 								
-								
-								
-		correctSolution = ""
+		 String correctSolution = ""
 								+ "123456789"
 								+ "123456789"
 								+ "123456789"
@@ -50,28 +41,13 @@ public class SudokuVerifierTest {
 								+ "123456789"
 								+ "123456789";
 		
-	//	int result = verifier.verify(candidateSolution );
-	//	assertThat(result, anyOf(is(-2), is(-3)));
+		
+		int result = verifier.verify(candidateSolution );
+		assertThat(result, anyOf(is(-2), is(-3)));
 		
 		
 	}
 
-	public SudokuVerifier getVerifier() {
-		return verifier;
-	}
-
-	public void setVerifier(SudokuVerifier verifier) {
-		this.verifier = verifier;
-	}
-
-	public String getCandidateSolution() {
-		return candidateSolution;
-	}
-
-	public void setCandidateSolution(String candidateSolution) {
-		this.candidateSolution = candidateSolution;
-	}
-	
 
 
 }
