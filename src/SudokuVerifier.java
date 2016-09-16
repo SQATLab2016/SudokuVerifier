15472c16628b75241611a89c8935e2d9e5021498
somename <someemail>
2016-09-16 15:44:27 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -5,11 +5,10 @@ public class SudokuVerifier {
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
-		/*
-		 * Return 0: means it is a valid Sudoku solution
-o Return -1: means it is violating Rule #1
-o Return -2: means it is violating Rule #2
-o Return -3: means it is violating Rule #3
-o Return -4: means it is violating Rule #4*/
+		/* test positive digits */
+		for (int i=0;i < candidateSolution.length;i++) 
+		{ 
+		    System.out.println(candidateSolution.substring(i,i)); 
+		} 
 	}
 }
