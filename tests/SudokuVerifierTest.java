07fac961089f43214f0f365ab009c56d9d83dfa5
somename <someemail>
2016-09-16 15:31:05 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -13,4 +13,11 @@ public class SudokuVerifierTest {
 		int t = Sudoku.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
 		assertEquals(0,t);
 	}
+	
+	@Test
+	public void testVerify_String2_minus1() throws StringLengthNotEqual_81Exception{
+		SudokuVerifier Sudoku = new SudokuVerifier();
+		int t = Sudoku.verify(" 123456789912345678891234567789123456678912345567891234456789123345678912234567891");
+		assertEquals(0,t);
+	}
 }
