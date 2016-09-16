154d179a6eb9888b3a9b770060594a0703c825e8
somename <someemail>
2016-09-16 15:26:33 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -7,6 +7,9 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String incorrectSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
 	@Test
 	public void test() {
 		SudokuVerifier verifier = new SudokuVerifier();
@@ -18,11 +21,9 @@ public class SudokuVerifierTest {
 	@Test
 	public void test_RowColumnViolation() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String candidateSolution = "";
-		
-		String correctSolution = ""
+		String candidateSolution = ""
 				+ "123456789"
-				+ "123456789"
+				+ "345678912"
 				+ "123456789"
 				+ "123456789"
 				+ "123456789"
@@ -30,6 +31,8 @@ public class SudokuVerifierTest {
 				+ "123456789"
 				+ "123456789"
 				+ "123456789";
+		
+		String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293;
 		
 		int result = verifier.verify(candidateSolution);
 		assertThat(result, anyOf(is(-2), is(-3)));
