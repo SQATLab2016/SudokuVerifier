1565fa1622bb825e9c9bf8edb3dc9a5cd52fdf84
somename <someemail>
2016-09-16 15:29:18 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 
 public class SudokuVerifierTest {
 
-// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
-// An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
+	public static final int RULE_1_VOILATION_RESULT = -1;
+	public static final int RULE_2_VOILATION_RESULT = -2;
+	public static final int RULE_3_VOILATION_RESULT = -3;
+	public static final int RULE_4_VOILATION_RESULT = -4;
+	
 	SudokuVerifier sudokuVerifier = new SudokuVerifier();
 	
 	@Test
@@ -17,4 +20,5 @@ public class SudokuVerifierTest {
 	public void testVerify_NumberCheckRule1(){
 		assertEquals("",-1,sudokuVerifier.verify("417369825032158947958720316825437169791586432346912758289643571573291684164875293"));
 	}
+	
 }
