fa72f10b1cc813d5d777e63cc54a9e9cfb47c4f6
somename <someemail>
2016-09-16 15:25:10 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
@@ -7,6 +8,10 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	
+	String incorrectSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
 	@Test
 	public void testVerify() {
 		SudokuVerifier verifier = new SudokuVerifier();
@@ -35,5 +40,10 @@ public class SudokuVerifierTest {
 		assertThat(result, anyOf(is(-2), is(-3)));
 		
 		//
+	}
+	
+	@Test
+	public void testPostiveDigits() {
+		
 	}
 }
