3189e961443ffcc0df731c035f35499a225ae2f1
somename <someemail>
2016-09-16 14:59:51 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,15 +1,28 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class SudokuVerifierTest {
 
-// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
-// An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
+// A correct Sudoku string: 417369825|632158947|958724316|825437169|791586432|346912758|289643571|573291684|164875293
+// An incorrect Sudoku string: 123456789|912345678|891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
 	public void testVerify() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void test_FirstColumnCorrect(){
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825"
+		
+		
+		public int verify(String candidateSolution)
+		
+		
+	}
+	
+	
 }
